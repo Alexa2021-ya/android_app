2c12051cffd695cd647396699ced9636b9fefd11
Alexa2021-ya <alexay0101@mail.ru>
2026-03-26 19:58:00 +0700

    Домашнее задание 14

--- app/src/main/java/com/example/myapplication/data/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/data/repository/AppRepositoryImpl.kt
@@ -1,28 +1,34 @@
 package com.example.myapplication.data.repository
 
-import com.example.myapplication.data.datasource.MockDataSource
 import com.example.myapplication.data.mapper.mapToDomain
 import com.example.myapplication.data.mapper.mapToDomainList
+import com.example.myapplication.data.network.api.ApiService
 import com.example.myapplication.domain.model.AppItem
 import com.example.myapplication.domain.repository.AppRepository
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class AppRepositoryImpl @Inject constructor() : AppRepository {
+class AppRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : AppRepository {
+
     override suspend fun getAppList(): List<AppItem> {
-        delay(500)
-        return MockDataSource.appListDto.mapToDomainList()
+        val response = apiService.getCatalog()
+        return response.mapToDomainList()
     }
 
     override suspend fun getAppById(id: String): AppItem? {
-        return MockDataSource.appListDto
-            .find { it.id == id }
-            ?.mapToDomain()
+        return try {
+            val details = apiService.getAppDetails(id)
+            details.mapToDomain()
+        } catch (e: Exception) {
+            null
+        }
     }
 
     override fun getAppListFlow(): Flow<List<AppItem>> = flow {
-        emit(MockDataSource.appListDto.mapToDomainList())
+        val list = getAppList()
+        emit(list)
     }
 }
